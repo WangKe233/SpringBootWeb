ea9b20c16f8fc6659d67d5259035e585dee8c272
WangKe233 <2195000850@qq.com>
2018-03-01 22:48:59 +0800

    第一个spring boot 项目

---
+++ src/main/java/com/example/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.example.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by WK on 2018/3/1.
+ */
+
+public interface UserDao  extends CrudRepository{
+
+}
---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by WK on 2018/3/1.
+ */
+@Service
+public class UserService {
+}
