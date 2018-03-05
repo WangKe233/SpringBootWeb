dab4060423fb7d1dffc2ee652015ebd6dfe900dd
WangKe233 <2195000850@qq.com>
2018-03-05 16:13:40 +0800

    登陆页面进行了轻微的修改

--- src/main/java/com/example/aspect/WebService.java
+++ src/main/java/com/example/aspect/WebService.java
@@ -5,7 +5,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 /**
  * Created by WK on 2018/3/5.
  */
-public class WebService  extends WebMvcConfigurerAdapter{
-//    @Override
-//    protected  void configure(AuthenticationManagerBuilder builder)
+/*
+public class WebService  extends WebSecurityConfigurerAdapter {
+
+
 }
+*/
